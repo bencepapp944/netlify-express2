985277d17178220f6e09fe07c3904d64780d7be9
Bence Papp <bence.papp@outlook.com>
2019-05-22 11:41:50 +0200

    Commit.

--- express/server.js
+++ express/server.js
@@ -1,35 +1,28 @@
 'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
 
-router.get('/', (req, res) => {
-  
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  res.write('<h1>Hello from Express.jsassasasss!</h1>');
-
-  res.end();
-
-});
-
-router.get('/picture', (req, res) => {
-
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  res.write('<h1>Picture</h1>');
-
-  res.end();
-
-});
-
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const app = express();
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+app.get('/', (req, res) => {
+  
+ 
+
+  res.send('<h1>Hello World!</h1>');
+
+});
+
+app.get('/picture', (req, res) => {
+
+  res.send("This is a picture.")
+
+});
+
+app.post('/', (req, res) => res.json({ postBody: req.body }));
 
 module.exports = app;
 module.exports.handler = serverless(app);
